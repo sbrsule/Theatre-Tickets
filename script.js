5eaeb05bdce690f5d6629405bdfdac765efa386a
Sabr <sbrsuleiman@gmail.com>
2021-08-03 21:30:44 -0400

    Added local storage and clear button

--- script.js
+++ script.js
@@ -3,27 +3,69 @@ const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
-let ticketPrice = parseInt(movieSelect.value);
- 
+const clearAll = document.getElementById('clearButton');
+
+populateUI();
+
+let ticketPrice = +movieSelect.value;
+
+// Save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
 
-    const seatsIndex = [...selectedSeats].map();
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
 
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
     const selectedSeatsCount = selectedSeats.length;
-
+    console.log(seatsIndex);
     count.innerText = selectedSeatsCount;
     total.innerText = selectedSeatsCount * ticketPrice;
+
+    if(selectedSeatsCount > 0) {
+        clearAll.classList.add('visible');
+    }
+    else {
+        clearAll.classList.remove('visible');
+    }
 }
 
-function updatePrice() {
-
+function clear() {
+    localStorage.clear();
 }
 
+// Get data from localstorage
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+    if(selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if(selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('selected');
+            }
+        })
+    }
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+    if(selectedMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
+}
+
+clearAll.addEventListener('click', () => {
+    seats.forEach((seat) => {
+        if(seat.classList.contains('selected')) {
+            seat.classList.remove('selected');
+        }
+    })
+    localStorage.clear();
+    updateSelectedCount();
+})
 //Movie selector event
 movieSelect.addEventListener('change', function(e) {
     ticketPrice = parseInt(e.target.value);
-
+    setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectedCount();
 })
 
@@ -34,4 +76,4 @@ container.addEventListener('click', function(e) {
     }
 })
 
-
+updateSelectedCount();
